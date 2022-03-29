0c4ea17ad635da159bc963b0a6f9200d27a0054e
Ally Netzel <netzelally@gmail.com>
2022-03-29 10:45:50 -0600

    commit tree 2cb9711979b6eb08712fe58c565103da94b6ba82

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function findMinAndRemoveSorted(array) {
+	let newMin;
+	let sorted = [];
+	while (array.length != 0) {
+		newMin = findMinAndRemoveSorted(array);
+		sorted.push(newMin);
+	}
+	return sorted;
+}
+let firstHalf =  [1, 2, 6, 7, 8]
+let secondHalf =  [3, 4, 5, 9, 10]
 
+function merge(firstHalf, secondHalf){
+	let sorted = []
+	while(firstHalf.length != 0 && secondHalf.length != 0){
+		let currentMin = findMinAndRemoveSorted(firstHalf, secondHalf)
+		sorted.push(currentMin)
+	}
+	return sorted.concat(firstHalf).concat(secondHalf)
+}	
